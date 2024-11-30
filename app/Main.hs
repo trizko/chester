5218109780b366f74a416ea9fd0050587e95cfd1
Tony Rizko <tony@rizkocircle.com>
2024-11-29 22:31:40 -0800

    Add types for tracking game state

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,8 @@
 module Main where
 
+type PlayerTimer = Int
+data Side = White | Black deriving Show
+data Game = Game { white :: PlayerTimer, black :: PlayerTimer, side :: Side } deriving Show
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
