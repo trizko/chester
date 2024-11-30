054bd19bea687acae120a1ce2d29253d9dbd2be9
Tony Rizko <tony@rizkocircle.com>
2024-11-29 22:35:11 -0800

    Add recursive playTurn function to start timer

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever, when)
+import System.IO (hSetBuffering, stdin, BufferMode(NoBuffering))
+
 type PlayerTimer = Int
 data Side = White | Black deriving Show
 data Game = Game { white :: PlayerTimer, black :: PlayerTimer, side :: Side } deriving Show
@@ -12,5 +16,16 @@ toggleTurn :: Game -> Game
 toggleTurn (Game w b White) = Game { white = w, black = b, side = Black}
 toggleTurn (Game w b Black) = Game { white = w, black = b, side = White}
 
+playTurn :: Game -> IO ()
+playTurn game = do
+    print game
+    when (white game > 0 && black game > 0) $ do
+        threadDelay 1000000
+        let newGame = decrement game
+        playTurn newGame
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let game = Game { white = 10, black = 10, side = White }
+    putStrLn "Starting the chess timer. Press space bar to toggle turns."
+    playTurn game
